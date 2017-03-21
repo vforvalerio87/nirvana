e3ca3b1471107de3d3c4fcd29b1b394d105f92bd
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-03-21 21:08:25 +0100

    re-render automatico su delete e set

--- src/static/app.js
+++ src/static/app.js
@@ -1,8 +1,7 @@
 const root = document.getElementById('root')
 
 function renderToDOM (componentPromise, target) {
-  const promise = componentPromise()
-  promise.then(component => {
+  componentPromise().then(component => {
     target.innerHTML = component
   })
 }
@@ -16,6 +15,7 @@ const stateAccessor = new Proxy(
   state,
   {
     get: (target, propKey) => {
+      console.log(`GET ${propKey}`)
       if (propKey in target) {
         console.log(`propKey "${propKey}" found in target; getting from state`)
         return Promise.resolve(Reflect.get(target, propKey))
@@ -24,12 +24,27 @@ const stateAccessor = new Proxy(
         return fetch('./api/index.json')
           .then(response => response.json())
           .then(json => {
-            target[propKey] = json[propKey]
+            console.log(`Saving "${propKey}" in target`)
+            state[propKey] = json[propKey]
           })
           .then(() => new Promise(resolve => {
             resolve(Reflect.get(target, propKey))
           }))
       }
+    },
+    set: (target, propKey, propValue) => {
+      console.log(`SET ${propKey} to ${propValue}`)
+      Reflect.set(target, propKey, propValue)
+      renderToDOM(Home, root)
+    },
+    deleteProperty: (target, propKey) => {
+      console.log(`DELETE ${propKey}`)
+      if (propKey in target) {
+        console.log(`propKey "${propKey}" found in target; deleting`)
+        Reflect.deleteProperty(target, propKey)
+        renderToDOM(Home, root)
+      }
+      else console.log(`${propKey} not found in target; doing nothing`)
     }
   }
 )
@@ -51,28 +66,33 @@ const MessageBar = () => new Promise(resolve => {
 
 const RefreshButton = () => Promise.resolve(`<button onclick="renderToDOM(Home, root)">Refresh</button>`)
 
-const DeleteButton = () => Promise.resolve(`<button onclick="delete state.message">Delete</button>`)
+const DeleteButton = () => Promise.resolve(`<button onclick="delete stateAccessor.message">Delete</button>`)
+
+const ChangeMessageButton = () => Promise.resolve(`<button onclick="stateAccessor.message='Sono il signore del male'">Change message</button>`)
 
 const Home = () => Promise.all([
   Header(),
   Body(),
   MessageBar(),
   RefreshButton(),
-  DeleteButton()
+  DeleteButton(),
+  ChangeMessageButton()
 ])
   .then(([
     headerElement,
     bodyElement,
     messageBarElement,
     refreshButtonElement,
-    deleteButtonElement
+    deleteButtonElement,
+    changeMessageButtonElement
   ]) => new Promise(resolve => {
     resolve(
       `${headerElement}
       ${bodyElement}
       ${messageBarElement}
       ${refreshButtonElement}
-      ${deleteButtonElement}`
+      ${deleteButtonElement}
+      ${changeMessageButtonElement}`
     )})
   )
 
